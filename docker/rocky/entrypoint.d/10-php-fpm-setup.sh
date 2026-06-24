0dc4efb365d9f3564e4e917b5eecdade4b287531
snowdream <snowdream@users.noreply.github.com>
2026-06-24 20:42:53 +0800

    refactor: enforce POSIX compliance by replacing \s with [[:space:]]

--- docker/rocky/entrypoint.d/10-php-fpm-setup.sh
+++ docker/rocky/entrypoint.d/10-php-fpm-setup.sh
@@ -4,24 +4,24 @@ set -e
 # Modifying configuration file php.ini
 # https://wiki.alpinelinux.org/wiki/Nginx_with_PHP
 find /etc  -name "php.ini" \
--exec sed -i "s|;*\s*display_errors\s*=\s*.*|display_errors = ${PHP_DISPLAY_ERRORS}|g" {} \; \
--exec sed -i "s|;*\s*display_startup_errors\s*=\s*.*|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|g" {} \; \
--exec sed -i "s|;*\s*error_reporting\s*=\s*.*|error_reporting = ${PHP_ERROR_REPORTING}|g" {} \; \
--exec sed -i "s|;*\s*memory_limit =\s*.*|memory_limit = ${PHP_MEMORY_LIMIT}|g" {} \; \
--exec sed -i "s|;*\s*upload_max_filesize =\s*.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|g" {} \; \
--exec sed -i "s|;*\s*max_file_uploads =\s*.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|g" {} \; \
--exec sed -i "s|;*\s*max_execution_time =\s*.*|max_execution_time = ${PHP_MAX_EXECUTION_TIME}|g" {} \; \
--exec sed -i "s|;*\s*max_input_time =\s*.*|max_input_time = ${PHP_MAX_INPUT_TIME}|g" {} \; \
--exec sed -i "s|;*\s*post_max_size =\s*.*|post_max_size = ${PHP_MAX_POST}|g" {} \; \
--exec sed -i "s|;*\s*cgi.fix_pathinfo=\s*.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|g" {} \; 
+-exec sed -i "s|;*[[:space:]]*display_errors[[:space:]]*=[[:space:]]*.*|display_errors = ${PHP_DISPLAY_ERRORS}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*display_startup_errors[[:space:]]*=[[:space:]]*.*|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*error_reporting[[:space:]]*=[[:space:]]*.*|error_reporting = ${PHP_ERROR_REPORTING}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*memory_limit =[[:space:]]*.*|memory_limit = ${PHP_MEMORY_LIMIT}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*upload_max_filesize =[[:space:]]*.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*max_file_uploads =[[:space:]]*.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*max_execution_time =[[:space:]]*.*|max_execution_time = ${PHP_MAX_EXECUTION_TIME}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*max_input_time =[[:space:]]*.*|max_input_time = ${PHP_MAX_INPUT_TIME}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*post_max_size =[[:space:]]*.*|post_max_size = ${PHP_MAX_POST}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*cgi.fix_pathinfo=[[:space:]]*.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|g" {} \; 
 
 # Modifying configuration file www.conf
 # https://wiki.alpinelinux.org/wiki/Nginx_with_PHP
 find /etc  -name "www.conf" \
--exec sed -i "s|;*\s*listen.owner\s*=\s*.*|listen.owner = ${PHP_FPM_USER}|g" {} \; \
--exec sed -i "s|;*\s*listen.group\s*=\s*.*|listen.group = ${PHP_FPM_GROUP}|g" {} \; \
--exec sed -i "s|;*\s*listen\s*=\s*.*|listen = ${PHP_FPM_HOST}:${PHP_FPM_PORT}|g" {} \; \
--exec sed -i "s|;*\s*listen.mode\s*=\s*.*|listen.mode = ${PHP_FPM_LISTEN_MODE}|g" {} \; \
--exec sed -i "s|;*\s*user\s*=\s*.*|user = ${PHP_FPM_USER}|g" {} \; \
--exec sed -i "s|;*\s*group\s*=\s*.*|group = ${PHP_FPM_GROUP}|g" {} \; \
--exec sed -i "s|;*\s*log_level\s*=\s*.*|log_level = notice|g" {} \;
+-exec sed -i "s|;*[[:space:]]*listen.owner[[:space:]]*=[[:space:]]*.*|listen.owner = ${PHP_FPM_USER}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*listen.group[[:space:]]*=[[:space:]]*.*|listen.group = ${PHP_FPM_GROUP}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*listen[[:space:]]*=[[:space:]]*.*|listen = ${PHP_FPM_HOST}:${PHP_FPM_PORT}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*listen.mode[[:space:]]*=[[:space:]]*.*|listen.mode = ${PHP_FPM_LISTEN_MODE}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*user[[:space:]]*=[[:space:]]*.*|user = ${PHP_FPM_USER}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*group[[:space:]]*=[[:space:]]*.*|group = ${PHP_FPM_GROUP}|g" {} \; \
+-exec sed -i "s|;*[[:space:]]*log_level[[:space:]]*=[[:space:]]*.*|log_level = notice|g" {} \;
